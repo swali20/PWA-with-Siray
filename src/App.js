efec9baabbd197874e0d219d812232bc84487e90
Siray Wali <school@Sirays-Mac-mini.local>
2021-04-08 05:52:44 -0500

    fix: styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, { useContext } from 'react';
 export default function App() {
   const size = useContext(ResponsiveContext);
   return (
-    <Header height={size}>
+    <Header justify="center" align="center" direction="column" height="small">
       <Heading level={1}>PWA with Siray</Heading>
       <Heading level={3}>Current screen size: {size}</Heading>
     </Header>
